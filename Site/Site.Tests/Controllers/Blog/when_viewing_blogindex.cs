7249976b720639e817fa685f25fdf83234535741
Craig Vermeer <craig@vermeers.net>
2010-01-21 15:26:08 -0500

    Couple tests, change a bit of wording on the view

--- Site/Site.Tests/Controllers/Blog/when_viewing_blogindex.cs
+++ Site/Site.Tests/Controllers/Blog/when_viewing_blogindex.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
+using NUnit.Framework;
 using Site.Controllers;
+using Site.Model.Entities;
 
 namespace Site.Tests.Controllers.Blog
 {
@@ -16,8 +18,22 @@ namespace Site.Tests.Controllers.Blog
             result = controller.Index();
         }
 
+        [Test]
+        public void ViewData_Should_Contain_Entries()
+        {
+            var view = result as ViewResult;
+            view.ViewData["entries"].ShouldNotBeNull();
+        }
+
+        [Test]
+        public void Entries_Should_Be_BlogEntries()
+        {
+            var view = result as ViewResult;
+            view.ViewData["entries"].ShouldBeOfType(typeof (IQueryable<BlogEntry>));
+        }
 
 
-        
+
+
     }
 }
